c4e83f0d406622378803d921fd4cc826b9a08cf1
Miguel Angel Sanchez Papalotzi <msanchezpapalot@gmail.com>
2025-10-20 23:37:04 -0600

    aplicación de estilos en tarjetas de commentarios

--- src/components/PopularComments/index.tsx
+++ src/components/PopularComments/index.tsx
@@ -1,16 +1,17 @@
-import type { Comment } from '../../types/comments';
-import './PopularComments.css'
+import type { Comment } from "../../types/comments";
+import "./PopularComments.css";
 
 type PopularCommentsProps = {
   comments: Comment;
-}
+};
 
-export default function PopularComment({comments} : PopularCommentsProps) {
+export default function PopularComment({ comments }: PopularCommentsProps) {
   return (
-    <article className=''>
-      <h3>{comments.name}</h3>
-      {/* <p>{comments.body}</p> */}
+    <article className="todo-container">
+      <h3 className="todo-title">{comments.name}</h3>
+      <p className="todo-body">{comments.body}</p>
+      <p className="todo-email">{comments.email}</p>
       {/* <p><strong>Email:</strong> {comments.email}</p> */}
     </article>
-  )
+  );
 }
